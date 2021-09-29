773e1df5c786a0b99aac744b1122f292dbbbfce9
taki-21 <62042131+taki-21@users.noreply.github.com>
2021-09-29 23:18:10 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** TypeScriptの基本の型 */
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 123;
+
+// string
+let str: string = "A";
+
+// array
+let arr1: Array<number> = [1, 2, 3];
+let arr2: number[] = [3, 5, 6, 6];
+
+// tuple
+let tuple: [number, string] = [0, "dd"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = () => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "aaa" };
